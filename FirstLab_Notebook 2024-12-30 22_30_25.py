0024f9b9ae31e2452d36f25e5e5ec0844401b34f
gmuralidhar13@gmail.com <gmuralidhar13@gmail.com>
2025-01-01 11:25:19 +0000

    basic commands

---
+++ FirstLab_Notebook 2024-12-30 22_30_25.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+print("Hello World")                            
+print (1+2)
+
+# COMMAND ----------
+
+print ("Hello Python")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select "Hey Murali"
